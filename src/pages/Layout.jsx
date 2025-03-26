617ee8b8d2b17576e89245ad265e38e2080e6f4a
Himanshu Verma <himanshuverma544@gmail.com>
2025-03-26 12:22:07 +0530

    upd: removed the import of sidebar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from '../components/pages/layout/Header';
-import Sidebar from '../components/pages/layout/Sidebar';
 import Footer from '../components/pages/layout/Footer';
 
 
